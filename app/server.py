d017d1e40b0953915385f66902b62e62911a5902
Anne-Marie Roy <royam0820@gmail.com>
2020-07-23 13:42:08 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,6 +8,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+import json
+from fastai2_inference import Inferencer
+
+
 # export_file_url = YOUR_GDRIVE_LINK_HERE
 export_file_url = 'https://www.googleapis.com/drive/v3/files/1RKuRyjlLi4A7JNiZ9xZN1-U_WPXxMaOs?alt=media&key=AIzaSyDwW_lF9ZU9gguzewL_DocSKcmMEjk_CGc'
 export_file_name = 'export.pkl'
@@ -51,21 +55,62 @@ learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
 
-@app.route('/')
-async def homepage(request):
-    html_file = path / 'view' / 'index.html'
-    return HTMLResponse(html_file.open().read())
+##@app.route('/')
+##async def homepage(request):
+##    html_file = path / 'view' / 'index.html'
+##    return HTMLResponse(html_file.open().read()) */
+##
 
+##@app.route('/analyze', methods=['POST'])
+##async def analyze(request):
+##  img_data = await request.form()
+##  img_bytes = await (img_data['file'].read())
+##  img_np = np.array(Image.open(BytesIO(img_bytes)))
+##  pred = learn.predict(BytesIO(img_bytes))
+##  return JSONResponse({
+##      'result': str(pred[0])
+##  })
+##
 
-@app.route('/analyze', methods=['POST'])
+@app.route('/analyze:predict', methods=['POST'])
 async def analyze(request):
-  img_data = await request.form()
-  img_bytes = await (img_data['file'].read())
-  img_np = np.array(Image.open(BytesIO(img_bytes)))
-  pred = learn.predict(BytesIO(img_bytes))
-  return JSONResponse({
-      'result': str(pred[0])
-  })
+
+    data = await request.body()
+    data_json = json.loads(data)
+
+    if 'tta' in data_json:
+        tta = data_json['tta']
+    else:
+        tta = False
+            
+
+    try: 
+        start_time = time.time()
+
+        preds = inf.get_preds(data_json)
+        preds_dec, labels, probabilities = inf.get_results(preds)
+
+        inference_time = time.time() - start_time
+
+        # build response json
+        res_list = list(zip(labels,probabilities))
+        res_dicts = [{"label": d[0], "probability": d[1]} for d in res_list]
+        res = { 'predictions': res_dicts, 'tta': tta, "time": inference_time }
+        status = 200
+    except Exception as e:
+        error = str(e)
+        print('error: ' + error)
+        print(traceback.print_exc())
+        res = { "error": error }
+        status = 400
+
+    return JSONResponse(res, status_code=status)
+    
+
+@app.route('/analyze', methods=['GET'])
+def status(request):
+    return JSONResponse(dict(status='OK'))
+
 
 
 if __name__ == '__main__':
