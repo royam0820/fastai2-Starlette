44ea2139b4e9dd31c329148edec23ffff649239e
Anne-Marie Roy <royam0820@gmail.com>
2020-07-25 15:06:45 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -42,56 +42,7 @@ async def setup_learner():
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-            print(e)import aiohttp
-import asyncio
-import uvicorn
-from fastai2.vision.all import *
-from io import BytesIO
-from starlette.applications import Starlette
-from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import HTMLResponse, JSONResponse
-from starlette.staticfiles import StaticFiles
-
-import json
-from fastai2_inference import Inferencer
-
-
-# export_file_url = YOUR_GDRIVE_LINK_HERE
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1RKuRyjlLi4A7JNiZ9xZN1-U_WPXxMaOs?alt=media&key=AIzaSyDwW_lF9ZU9gguzewL_DocSKcmMEjk_CGc'
-export_file_name = 'export.pkl'
-
-# classes = YOUR_CLASSES_HERE
-classes = ['black', 'grizzly','teddy']
-path = Path(__file__).parent
-
-app = Starlette()
-app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory='app/static'))
-
-
-async def download_file(url, dest):
-    if dest.exists(): return
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f:
-                f.write(data)
-
-
-async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
-    try:
-        learn = torch.load(path/export_file_name, map_location=torch.device('cpu'))
-        learn.dls.device = 'cpu'
-        return learn
-    except RuntimeError as e:
-        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
             print(e)
-            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-            raise RuntimeError(message)
-        else:
-            raise
-
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
